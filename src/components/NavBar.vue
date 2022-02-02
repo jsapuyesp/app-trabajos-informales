0b4b32dd316a685c9e9cb9842b8745b65accebe0
Juan Sapuyes <jsapuyesp@gmail.com>
2022-02-02 16:24:30 -0500

    Retomando el proyecto

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,21 +1,26 @@
 <template>
-  <div>
-      <b-navbar type="dark" variant="dark">
-        <b-navbar-brand :to="{ name: 'Home' }">
-          <b-img src="https://vuejs.org/images/logo.png?_sw-precache=cf23526f451784ff137f161b8fe18d5a" style="max-width: 50px" fluid alt="Responsive image"></b-img>
-        </b-navbar-brand>
-        <b-navbar-nav style="margin-left: auto;">
-          <b-nav-item :to="{ name: 'Home' }">Home</b-nav-item>
+  <b-container fluid>
+    <b-navbar type="dark" variant="dark">
+      <b-navbar-brand :to="{ name: 'Home' }">
+        <b-img
+          src="https://vuejs.org/images/logo.png?_sw-precache=cf23526f451784ff137f161b8fe18d5a"
+          style="max-width: 50px"
+          fluid
+          alt="Responsive image"
+        ></b-img>
+      </b-navbar-brand>
+      <b-navbar-nav style="margin-left: auto">
+        <b-nav-item :to="{ name: 'Home' }">Home</b-nav-item>
 
-          <!-- Navbar dropdowns -->
-          <b-nav-item-dropdown text="User" right>
-            <b-dropdown-item :to="{ name: 'Profile' }">Profile</b-dropdown-item>
-            <b-dropdown-item :to="{ name: 'Login' }">Login</b-dropdown-item>
-            <b-dropdown-item :to="{ name: 'Register' }">Register</b-dropdown-item>
-          </b-nav-item-dropdown>
-        </b-navbar-nav>
-      </b-navbar>
-  </div>
+        <!-- Navbar dropdowns -->
+        <b-nav-item-dropdown text="User" right>
+          <b-dropdown-item :to="{ name: 'Profile' }">Profile</b-dropdown-item>
+          <b-dropdown-item :to="{ name: 'Login' }">Login</b-dropdown-item>
+          <b-dropdown-item :to="{ name: 'Register' }">Register</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-navbar>
+  </b-container>
 </template>
 
 <script>
@@ -27,10 +32,8 @@ export default {
 </script>
 
 <style scoped>
-.nav {
-  display: flex;
-}
-.auth {
-  margin-left: auto;
+.container-fluid {
+  padding-left: 0px;
+  padding-right: 0px;
 }
 </style>
