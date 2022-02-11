58f19f0e0cbaa4fe978c7b496c4a239ec7d73e99
Juan Sapuyes <jsapuyesp@gmail.com>
2022-02-11 00:44:48 -0500

    Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,15 +1,71 @@
 <template>
-  <div class="footer">
+  <div type="dark" class="footer p-3">
+    <b-row>
+      <b-col>
+        <b-card
+          class="b-card"
+          title="Card Title"
+          tag="article"
+          style="max-width: 20rem"
+        >
+          <b-card-text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </b-card-text>
 
+          <b-button href="#" variant="primary">Go somewhere</b-button>
+        </b-card>
+      </b-col>
+      <b-col>
+        <b-card
+          class="b-card"
+          title="Card Title"
+          tag="article"
+          style="max-width: 20rem"
+        >
+          <b-card-text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </b-card-text>
+
+          <b-button href="#" variant="primary">Go somewhere</b-button>
+        </b-card>
+      </b-col>
+      <b-col>
+        <b-card
+          class="b-card"
+          title="Card Title"
+          tag="article"
+          style="max-width: 20rem"
+        >
+          <b-card-text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </b-card-text>
+
+          <b-button href="#" variant="primary">Go somewhere</b-button>
+        </b-card>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
+.footer {
+  text-align: center;
+  text-justify: start;
+  background-color: #333333;
+  bottom: 0;
+  position: fixed !important;
+  width: 100%;
+}
 
+.b-card {
+  margin: 2px;
+  padding: 1px;
+}
 </style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
   <b-container fluid>
-    <b-navbar type="dark" variant="dark">
+    <b-navbar type="dark" class="b-navbar">
       <b-navbar-brand :to="{ name: 'Home' }">
         <b-img
           src="https://vuejs.org/images/logo.png?_sw-precache=cf23526f451784ff137f161b8fe18d5a"
@@ -35,5 +35,8 @@ export default {
 .container-fluid {
   padding-left: 0px;
   padding-right: 0px;
+}
+.b-navbar {
+  background-color: #333333;
 }
 </style>
